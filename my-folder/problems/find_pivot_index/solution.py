17b6cf6057fa8cfbc9386c3744c541c439592197
Leo <86375512+select-case@users.noreply.github.com>
2023-05-10 13:05:45 +0000

    Sync LeetCode submission - Find Pivot Index (python3)

--- my-folder/problems/find_pivot_index/solution.py
+++ my-folder/problems/find_pivot_index/solution.py
@@ -4,18 +4,13 @@ class Solution:
         sum_left = 0
         for i in nums:
             sum_right += i
-
         sum_right -= nums[0]
-        if sum_right == 0:
+        if sum_right == sum_left:
             return 0
-        for i in range(1, len(nums)):
-
+        for i in range(1,len(nums)):
             sum_right -= nums[i]
-            sum_left +=nums[i-1]
+            sum_left += nums[i-1]
             if sum_right == sum_left:
                 return i
-        # sum_right -= nums[-1]
-        # sum_left += nums[-2]
-        # if sum_right == sum_left:
-        #     return len(nums)
+         
         return -1
