ac1e8273e5e3676a3d1a300fa2bbbe696b36d816
Leo <86375512+select-case@users.noreply.github.com>
2023-06-09 11:36:48 +0530

    Create solution.py from https://practice.geeksforgeeks.org/problems/flattening-a-linked-list/1

---
+++ strivers-2023/problems/flattening_a_linked_list/solution.py
@@ -0,0 +1,27 @@
+def merge(a,b):
+    if a is None:
+        return b
+        
+    if b is None:
+        return a
+        
+    
+    result=None
+    
+    if a.data<b.data:
+        result=a
+        result.bottom=merge(a.bottom,b)
+    else:
+        result=b
+        result.bottom=merge(a,b.bottom)
+    return result
+    
+    
+
+def flatten(root):
+    if root is None or root.next is None:
+        return root
+        
+    root.next=flatten(root.next)
+    root=merge(root,root.next)
+    return root
