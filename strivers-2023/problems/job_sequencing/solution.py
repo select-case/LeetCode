26d49ebf78d72b5a975638c3de47d13f1989a192
Leo <86375512+select-case@users.noreply.github.com>
2023-06-11 10:49:26 +0530

    Create solution.py from https://practice.geeksforgeeks.org/problems/job-sequencing-problem-1587115620/1#

---
+++ strivers-2023/problems/job_sequencing/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    
+    #Function to find the maximum profit and the number of jobs done.
+    def JobScheduling(self,jobs,n):
+        jobs.sort(key=lambda x: x.profit, reverse=True)
+        maxi = jobs[0].deadline
+        for i in range(1, len(jobs)):
+            maxi = max(maxi, jobs[i].deadline)
+
+
+        slot = [-1] * (maxi + 1)
+        countJobs = 0
+        jobProfit = 0
+
+
+        for i in range(len(jobs)):
+            for j in range(jobs[i].deadline, 0, -1):
+                if slot[j] == -1:
+                    slot[j] = i
+                    countJobs += 1
+                    jobProfit += jobs[i].profit
+                    break
+
+
+        return countJobs, jobProfit
