8c79bbeff59913b0696756ee4fedc896c439b038
Leo <86375512+select-case@users.noreply.github.com>
2023-06-29 15:22:37 +0000

    Sync LeetCode submission - Valid Anagram (python3)

---
+++ strivers-2023/problems/valid_anagram/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        one = {}
+        two = {}
+        for i in s:
+            if i in one: one[i] += 1
+            else: one[i] = 1
+        for i in t:
+            if i in two: two[i] += 1
+            else: two[i] = 1
+        return one == two
