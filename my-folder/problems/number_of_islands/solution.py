7f9ba749da9fb17be951e3ac77af5cf411ebb8b3
Leo <86375512+select-case@users.noreply.github.com>
2023-09-19 17:12:53 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- my-folder/problems/number_of_islands/solution.py
+++ my-folder/problems/number_of_islands/solution.py
@@ -1,28 +1,21 @@
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        
-        if not grid:
-            return 0
+  def numIslands(self, grid):
+      if not grid:
+          return 0
+          
+      count = 0
+      for i in range(len(grid)):
+          for j in range(len(grid[0])):
+              if grid[i][j] == '1':
+                  self.dfs(grid, i, j)
+                  count += 1
+      return count
 
-        rows = len(grid)
-        cols = len(grid[0])
-        count = 0
-
-        def dfs(row, col):
-            if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] != '1':
-                return
-
-            grid[row][col] = '0'  
-
-            dfs(row + 1, col)  
-            dfs(row - 1, col)  
-            dfs(row, col + 1)  
-            dfs(row, col - 1)  
-
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == '1':
-                    count += 1
-                    dfs(i, j)
-
-        return count
+  def dfs(self, grid, i, j):
+      if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+          return
+      grid[i][j] = '#'
+      self.dfs(grid, i+1, j)
+      self.dfs(grid, i-1, j)
+      self.dfs(grid, i, j+1)
+      self.dfs(grid, i, j-1)
