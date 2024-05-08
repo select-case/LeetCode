b6a1874c3297a5291cf2471ef78c17d9b3d918e9
Leo <86375512+select-case@users.noreply.github.com>
2024-05-08 07:14:57 +0000

    Sync LeetCode submission Runtime - 67 ms (44.52%), Memory - 18.1 MB (10.68%)

---
+++ my-folder/0506-relative-ranks/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        lenScore = len(score)
+        answer = [0] * lenScore
+        maxHeap = []
+        rank = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+
+        for idx,s in enumerate(score):
+            heappush(maxHeap, (-s,idx))
+        
+        number = 1
+        while maxHeap:
+            s, index = heappop(maxHeap)
+            if number < 4:
+                answer[index] = rank[number - 1]
+            else:
+                answer[index] = str(number)
+            number += 1
+        return answer
+
