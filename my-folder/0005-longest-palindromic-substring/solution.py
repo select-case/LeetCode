dec1f1e5d1077d9a8aa66b273fe2b096c7d97073
Leo <86375512+select-case@users.noreply.github.com>
2024-06-27 16:08:30 +0000

    Sync LeetCode submission Runtime - 455 ms (49.39%), Memory - 16.7 MB (40.07%)

--- my-folder/0005-longest-palindromic-substring/solution.py
+++ my-folder/0005-longest-palindromic-substring/solution.py
@@ -1,17 +1,38 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        result, resultLen = "",0
-        for i in range(len(s)):
-            result, resultLen = computePalindrome(i,i, s, result, resultLen)
-            result, resultLen = computePalindrome(i,i+1, s, result, resultLen)
-        return result
-
-
-def computePalindrome(left, right, string, result, resultLen): 
-    while left >= 0 and right < len(string) and string[left] == string[right]:
-        if (right - left + 1) > resultLen:
-            result = string[left:right+1]
-            resultLen = right - left + 1
-        left -= 1
-        right += 1
-    return [result, resultLen]
+        max_len = 1
+        ans_str = s[0]
+        for base in range(len(s)):
+            left = 0
+            right = 0
+            while (base - left >= 0 and base + right < len(s)):
+                # print(ans_str,base,left,right)
+                if s[base - left] == s[base + right]:
+                    if 2*left + 1 > max_len:
+                        max_len = 2*left + 1
+                        ans_str = s[base - left:base + right + 1]
+                    left += 1
+                    right += 1
+                else:
+                    break
+        for base in range(0,len(s)-1):
+            left = 0
+            right = 0
+            if s[base] == s[base + 1]:
+                # print(ans_str,left,right)
+                while (base - left >= 0 and base + 1 + right < len(s)):
+                    if s[base - left] == s[base + right + 1]:
+                        if max_len < 2*left + 2:
+                            max_len = 2*left + 2
+                            ans_str = s[base - left:base + right + 2]
+                        left += 1
+                        right += 1
+                    else:
+                        break
+                        
+            
+            
+            
+        # print(max_len)
+        # print(ans_str)
+        return ans_str
