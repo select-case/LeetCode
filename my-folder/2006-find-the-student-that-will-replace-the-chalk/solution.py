b32a07969fe566002df4796e636321d94b6e452f
Leo <86375512+select-case@users.noreply.github.com>
2024-09-02 13:47:09 +0000

    Sync LeetCode submission Runtime - 565 ms (5.22%), Memory - 30.6 MB (30.45%)

---
+++ my-folder/2006-find-the-student-that-will-replace-the-chalk/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        sum_chalk = 0
+        for i in range(len(chalk)):
+            sum_chalk += chalk[i]
+            if sum_chalk > k:
+                break
+        k = k % sum_chalk
+        for i in range(len(chalk)):
+            if k < chalk[i]:
+                return i
+            k -= chalk[i]
+        return 0
